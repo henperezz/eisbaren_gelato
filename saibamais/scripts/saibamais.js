c7f75c3f48aef58342eb011bdac89a5efd792650
Stefani Alves <stefanipereiraalves8@gmail.com>
2026-04-28 11:07:42 -0300

    mudanças footer

--- saibamais/scripts/saibamais.js
+++ saibamais/scripts/saibamais.js
@@ -20,15 +20,7 @@ buttonSwitch.addEventListener("click", () => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-// Navbar transparente ao rolar
-window.addEventListener("scroll", function() {
-    const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
-        navbar.classList.add("transparent");
-    } else {
-        navbar.classList.remove("transparent");
-    }
-});
+
 
 // Remove splash após alguns segundos
 window.addEventListener("load", () => {
