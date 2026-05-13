b91c54c94fa2126c5d3d4750c3b9c68e09d8af25
Ester <esterhelena16@ahoo.com.br>
2026-05-12 21:28:22 -0300

    identação

--- html-css/scripts/scripts.js
+++ html-css/scripts/scripts.js
@@ -5,3 +5,9 @@ buttonSwitch.addEventListener("click", () => {
   buttonSwitch.classList.toggle("dark");
   /*headerContainer.classList.toggle("dark");*/
 });
+
+let favoriteIcon = document.getElementById("favoriteIcon");
+
+favoriteIcon.addEventListener("click", () => {
+  favoriteIcon.classList.toggle("favorited");
+});
