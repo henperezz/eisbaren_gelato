6e159327c9e8a46749e1f3bbf1a4e64c6dfa9ccd
henperezz <pedro.henrique.perez08@gmail.com>
2026-05-17 16:03:15 -0300

    minor changes, 1.2.0, add dropdown menu at additionals and quantity button, both not functional yet

--- html_css_js/productPages/scripts/ProductPage.js
+++ html_css_js/productPages/scripts/ProductPage.js
@@ -11,3 +11,28 @@ let favoriteIcon = document.getElementById("favoriteIcon");
 favoriteIcon.addEventListener("click", () => {
   favoriteIcon.classList.toggle("favorited");
 });
+
+let value = 0;
+
+document.querySelector(".buttonMinus").addEventListener("click", () => {
+  if (value == 0) {
+    return;
+  } else {
+    value--;
+  }
+  document.getElementById("valueQuantity").textContent = value;
+});
+
+document.querySelector(".buttonPlus").addEventListener("click", () => {
+  value++;
+  document.getElementById("valueQuantity").textContent = value;
+});
+
+let additionals = document.querySelector(".additionals");
+let additionalsList = document.querySelector(".additionalsList");
+let arrowRight = document.querySelector(".arrowRight");
+
+additionals.addEventListener("click", () => {
+  additionalsList.classList.toggle("activated");
+  arrowRight.classList.toggle("activated");
+});
