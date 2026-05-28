80ca2a7c5c3cdddef2fb75013c5098d8b61833b0
Pedro Henrique Perez <pedro.henrique.perez08@gmail.com>
2026-05-28 13:54:39 -0300

    fixing stuff 3.0

--- html_css_js/index/js/index.js
+++ html_css_js/index/js/index.js
@@ -4,8 +4,4 @@ let headerContainer = document.getElementById("headerContainer");
 buttonSwitch.addEventListener("click", () => {
   buttonSwitch.classList.toggle("dark");
   /*headerContainer.classList.toggle("dark");*/
-<<<<<<<< HEAD:html_css_js/index/js/index.js
 });
-========
-});
->>>>>>>> b57a2f69e4d800116ef629a3c98172da72e877a1:index.js/paginainicial.js
