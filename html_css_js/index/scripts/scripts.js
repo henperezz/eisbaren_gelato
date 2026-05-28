7f0e6e1707a5ce4b17b7fa11a31de5187bea43d3
Pedro Henrique Perez <pedro.henrique.perez08@gmail.com>
2026-05-28 13:07:52 -0300

    Revert "Merge branch 'front-index'"
    This reverts commit f86232996feb7ee75f6a65acf9161fdb235a08ef, reversing
    changes made to c18c95bd918289efd9275e79aac44923db7373c9.

--- html_css_js/index/scripts/scripts.js
+++ html_css_js/index/scripts/scripts.js
@@ -1,14 +1,2 @@
-let buttonSwitch = document.getElementById("buttonSwitch");
-let headerContainer = document.getElementById("headerContainer");
 
-buttonSwitch.addEventListener("click", () => {
-  buttonSwitch.classList.toggle("dark");
-  /*headerContainer.classList.toggle("dark");*/
-});
-
-let favoriteIcon = document.getElementById("favoriteIcon");
-
-favoriteIcon.addEventListener("click", () => {
-  favoriteIcon.classList.toggle("favorited");
-});
 
